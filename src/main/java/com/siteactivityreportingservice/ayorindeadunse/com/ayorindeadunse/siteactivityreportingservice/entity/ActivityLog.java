7102fc183e62c0fe3e2d085b2711f7cec83e1808
ayorindeadunse <adunseayorinde@gmail.com>
2022-06-01 21:57:55 +0100

    Added ActivityLog entity

---
+++ src/main/java/com/siteactivityreportingservice/ayorindeadunse/com/ayorindeadunse/siteactivityreportingservice/entity/ActivityLog.java
@@ -0,0 +1,28 @@
+package com.siteactivityreportingservice.ayorindeadunse.com.ayorindeadunse.siteactivityreportingservice.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+
+@Entity
+public class ActivityLog {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private int id;
+   private String activityName;
+   private int activityValue;
+   private Date dateLogged;
+   private Date dateModified;
+}
