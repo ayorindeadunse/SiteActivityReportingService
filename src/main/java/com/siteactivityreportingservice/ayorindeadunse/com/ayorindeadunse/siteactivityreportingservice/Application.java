af8c572cd3370653a058542d34d4b3c6ee8d7efa
ayorindeadunse <adunseayorinde@gmail.com>
2022-06-01 21:49:10 +0100

    Adding initialized project files

---
+++ src/main/java/com/siteactivityreportingservice/ayorindeadunse/com/ayorindeadunse/siteactivityreportingservice/Application.java
@@ -0,0 +1,13 @@
+package com.siteactivityreportingservice.ayorindeadunse.com.ayorindeadunse.siteactivityreportingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
