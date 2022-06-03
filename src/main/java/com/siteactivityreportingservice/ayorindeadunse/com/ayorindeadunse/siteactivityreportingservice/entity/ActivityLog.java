1eeb48fd1b3d14747ac8b2e89e6e4fe9495e1497
ayorindeadunse <adunseayorinde@gmail.com>
2022-06-03 17:56:15 +0100

    updated ActivityLog pojo

--- src/main/java/com/siteactivityreportingservice/ayorindeadunse/com/ayorindeadunse/siteactivityreportingservice/entity/ActivityLog.java
+++ src/main/java/com/siteactivityreportingservice/ayorindeadunse/com/ayorindeadunse/siteactivityreportingservice/entity/ActivityLog.java
@@ -4,11 +4,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.util.Date;
 
 @Getter
